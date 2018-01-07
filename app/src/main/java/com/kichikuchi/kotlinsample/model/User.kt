0f710cd79476e1710da28d0e1be2f44b99de0cff
kichikuchi <kikuchi0111@gmail.com>
2018-01-07 21:45:38 +0900

    create User

---
+++ app/src/main/java/com/kichikuchi/kotlinsample/model/User.kt
@@ -0,0 +1,9 @@
+package com.kichikuchi.kotlinsample.model
+
+/**
+ * Created by KazunoriKikuchi on 2018/01/07.
+ */
+
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
