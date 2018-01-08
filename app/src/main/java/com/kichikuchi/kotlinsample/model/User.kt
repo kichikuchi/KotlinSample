3b47e4fc6cbab38fb5001e90d82dbae7a5ce49a8
kichikuchi <kikuchi0111@gmail.com>
2018-01-08 12:10:39 +0900

    UserをParcelableに

--- app/src/main/java/com/kichikuchi/kotlinsample/model/User.kt
+++ app/src/main/java/com/kichikuchi/kotlinsample/model/User.kt
@@ -4,6 +4,31 @@ package com.kichikuchi.kotlinsample.model
  * Created by KazunoriKikuchi on 2018/01/07.
  */
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class User(val id: String,
                 val name: String,
-                val profileImageUrl: String)
+                val profileImageUrl: String) : Parcelable {
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<User> = object : Parcelable.Creator<User> {
+            override fun createFromParcel(source: Parcel): User = source.run {
+                User(readString(), readString(), readString())
+            }
+
+            override fun newArray(size: Int): Array<User?> = arrayOfNulls(size)
+        }
+    }
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.run {
+            writeString(id)
+            writeString(name)
+            writeString(profileImageUrl)
+        }
+    }
+}
