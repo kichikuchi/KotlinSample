89cbf8dbc5c1c0384532fb93561b48cb5ab10b1d
kichikuchi <kikuchi0111@gmail.com>
2018-01-08 12:21:28 +0900

    ArticleをParcelableに

--- app/src/main/java/com/kichikuchi/kotlinsample/model/Article.kt
+++ app/src/main/java/com/kichikuchi/kotlinsample/model/Article.kt
@@ -4,7 +4,32 @@ package com.kichikuchi.kotlinsample.model
  * Created by KazunoriKikuchi on 2018/01/07.
  */
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Article(val id: String,
                    val title: String,
                    val url: String,
-                   val user: User)
+                   val user: User) : Parcelable {
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Article> = object : Parcelable.Creator<Article> {
+            override fun createFromParcel(source: Parcel): Article = source.run {
+                Article(readString(), readString(), readString(), readParcelable(Article::class.java.classLoader))
+            }
+
+            override fun newArray(size: Int): Array<Article?> = arrayOfNulls(size)
+        }
+    }
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.run {
+            writeString(id)
+            writeString(title)
+            writeString(url)
+            writeParcelable(user, flags)
+        }
+    }
+}
