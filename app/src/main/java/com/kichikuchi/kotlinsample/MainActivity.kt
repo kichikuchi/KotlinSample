9813759f2fc98d70d1de2e9a07e6958e82ceb8de
kichikuchi <kikuchi0111@gmail.com>
2018-01-08 19:19:33 +0900

    use inject

--- app/src/main/java/com/kichikuchi/kotlinsample/MainActivity.kt
+++ app/src/main/java/com/kichikuchi/kotlinsample/MainActivity.kt
@@ -8,49 +8,37 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.ProgressBar
-import com.google.gson.FieldNamingPolicy
-import com.google.gson.GsonBuilder
 import com.kichikuchi.kotlinsample.model.Article
 import com.kichikuchi.kotlinsample.model.User
 import com.kichikuchi.kotlinsample.client.ArticleClient
 import io.reactivex.android.schedulers.AndroidSchedulers
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var articleClient: ArticleClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        (application as QiitaClientApp).component.inject(this)
         setContentView(R.layout.activity_main)
 
         val listAdapter = ArticleListAdapter(applicationContext)
         listAdapter.articles = listOf(dummyArticle("kotlin入門", "太郎"), dummyArticle("java入門", "二郎"))
 
-        val listView: ListView = findViewById<ListView>(R.id.list_view)
+        val listView = findViewById<ListView>(R.id.list_view)
+        val queryEditText = findViewById<EditText>(R.id.query_edit_text)
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+
         listView.adapter = listAdapter
         listView.setOnItemClickListener { adapterView, view, position, id ->
             val article = listAdapter.articles[position]
             ArticleActivity.intent(this, article).let { startActivity(it) }
         }
-
-        val gson = GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .create()
-
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://qiita.com")
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-
-        val articleClient = retrofit.create(ArticleClient::class.java)
-
-        val queryEditText = findViewById<EditText>(R.id.query_edit_text)
-        val searchButton = findViewById<Button>(R.id.search_button)
-        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
 
         searchButton.setOnClickListener {
             progressBar.visibility = View.VISIBLE
