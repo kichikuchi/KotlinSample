bbb838f3affb93796eaf6eac5f0970317e7a0d0e
kichikuchi <kikuchi0111@gmail.com>
2018-01-08 16:44:41 +0900

    create ArticleClient

---
+++ app/src/main/java/com/kichikuchi/kotlinsample/client/ArticleClient.kt
@@ -0,0 +1,16 @@
+package com.kichikuchi.kotlinsample.client
+
+/**
+ * Created by KazunoriKikuchi on 2018/01/08.
+ */
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import rx.Observable
+import com.kichikuchi.kotlinsample.model.Article
+
+interface ArticleClient {
+
+    @GET("/api/v2/items")
+    fun search(@Query("query") query: String): Observable<List<Article>>
+}
